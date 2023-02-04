ef81d3545a05c0884dcb8e9eec42361730a437d2
Fernley <fernley1952@gmail.com>
2023-02-05 00:19:15 +0300

    Menu sounds added.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,24 +6,29 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu, optionMenu, creditsMenu;
+    public AudioSource clickSound;
    public void PlayGame()
    {
+        clickSound.Play();
         SceneManager.LoadScene("Game");
    }
 
     public void OptionMenu()
     {
+        clickSound.Play();
         optionMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
 
     public void CreditsMenu()
     {
+        clickSound.Play();
         creditsMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
     public void ReturnMainMenu()
     {
+        clickSound.Play();
         mainMenu.SetActive(true);
         creditsMenu.SetActive(false);
         optionMenu.SetActive(false);
